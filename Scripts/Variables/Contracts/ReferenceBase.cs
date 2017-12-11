d563c024d85bec3f660299cf4c16a4188f6469c3
Kyappy <m.gavanier@gmail.com>
2017-12-11 18:13:42 +0000

    Reference and Variable base classes added

---
+++ Scripts/Variables/Contracts/ReferenceBase.cs
@@ -0,0 +1,33 @@
+﻿namespace InjectionModule.Variables.Contracts {
+	/// <summary>
+	/// Reference base support.
+	/// </summary>
+	public abstract class ReferenceBase<TType, TVariable> where TVariable : VariableBase<TType> {
+		#region Public fields		
+		/// <summary>
+		/// Use a constant value.
+		/// </summary>
+		public bool UseConstant = true;
+
+		/// <summary>
+		/// The constant value.
+		/// </summary>
+		public TType ConstantValue;
+
+		/// <summary>
+		/// The variable float.
+		/// </summary>
+		public TVariable Variable;
+		#endregion
+
+		#region Public getters		
+		/// <summary>
+		/// Gets the value.
+		/// </summary>
+		/// <value>
+		/// The constant value if it should be used, else the scriptable variable value.
+		/// </value>
+		public TType Value { get { return UseConstant ? ConstantValue : Variable.Value; } }
+		#endregion
+	}
+}
---
+++ Scripts/Variables/Contracts/VariableBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace InjectionModule.Variables.Contracts {
+	/// <summary>
+	/// Scriptable variable base.
+	/// </summary>
+	/// <seealso cref="ScriptableObject"/>
+	public class VariableBase<T> : ScriptableObject {
+		#region Public fields
+		/// <summary>
+		/// The variable value.
+		/// </summary>
+		public T Value;
+		#endregion
+	}
+}
