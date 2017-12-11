f4a0b52e851e410de582fc05f6ab80b13fda9481
Kyappy <m.gavanier@gmail.com>
2017-12-11 18:18:04 +0000

    Editor supports reference type added

---
+++ Editor/Supports/ReferenceType.cs
@@ -0,0 +1,16 @@
+﻿namespace Modules.InjectionModule.Supports {
+	/// <summary>
+	/// The reference type.
+	/// </summary>
+	internal enum ReferenceType {
+		/// <summary>
+		/// Use constant reference type.
+		/// </summary>
+		UseConstant,
+
+		/// <summary>
+		/// Use variable reference type.
+		/// </summary>
+		UseVariable
+	}
+}
